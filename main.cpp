87f627e91e0c1a710c2b68cadb904ac9a6fd4f5b
luis sebastian <luissebastian1132014@gmail.com>
2020-07-23 17:25:54 +0000

    arreglo pares

--- main.cpp
+++ main.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
 using namespace std;
 void ingresar_arreglo (int a[],int l);
-void imprimir(int a[],int b[], int i,int j,int k, int l);
+void imprimir(int a[],int b[], int l);
 
 int main()
 {
-  int a[100], b[100], i, j = 0, k =0,l; 
+  int a[100], b[100], l;
   cout<<"Ingrese el tamaño del arreglo : " ;
   cin >> l;
   ingresar_arreglo (a,l);
   cout<< endl;
-  imprimir (a,b,i,j,k, l);
+  imprimir (a,b,l);
   cout<< endl;
   return 0;
-}
-void ingresar_arreglo (int a[], int i, int l);
-{
-  int  i, j = 0, k =0,l;
-  for ( i=0; i<5; i++)
+ }
+ void ingresar_arreglo (int a[],int l) 
+ {
+  int  i, j = 0;
+  for ( i=0; i<l; i++)
    {
    cout<<"ingrese["<< i<<"]=";
    cin >> a[i];
    }
    
-}
- 
-void imprimir(int a[],int b[], int i,int j,int k, int l);
-{  
-  for (i  =0;i < 5; i++)
+ }
+ void imprimir(int a[],int b[],int l)
+ {  
+  int i, k=0, m=0;
+  for (i  =0;i < l; i++)
   {
-  if (a [i]%2==0)
-  {
-  b[j]= a[i];
-  j++;
-  }
+   if (a [i]%2==0)
+   {
+   b[k]= a[i];
+   k++;
+   m++;
+   }
   }
   cout<< "Los numeros pares son:"<< endl;
-  for (i=0;i<j; i++)
-  {
-   cout <<"["<<i<<"]"<<b[i];
-  }
-  return 0;
-  } 
+  for (i=0;i<m; i++)
+   {
+   cout <<"["<<i<<"]"<<b[i]<< endl;
+   }
 }
